2d24ecfeeade9a5d3ad3a4634336c442716e3490
Work <alexnguyen@Alexs-MacBook-Air.local>
2025-11-10 18:39:43 +0700

    infra provisioning using terraform

---
+++ terraform/regions/us-east-1/main.tf
@@ -0,0 +1,17 @@
+# main.tf
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "example" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
+  # subnet_id            = var.subnet_id  # Uncomment if using
+
+  tags = {
+    Name = var.instance_name
+  }
+}
