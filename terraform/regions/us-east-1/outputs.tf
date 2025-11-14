57a29de5a8083b26b2aec55436ba0088baa120ab
Work <alexnguyen@Alexs-MacBook-Air.local>
2025-11-15 00:51:23 +0700

    fix output variable

--- terraform/regions/us-east-1/outputs.tf
+++ terraform/regions/us-east-1/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
-  value = aws_instance.app[0].public_ip
+  value = length(data.aws_instances.existing.ids) > 0 ? aws_instance.app[0].public_ip : ""
 }
 
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value = aws_instance.app[0].id
+  value = length(data.aws_instances.existing.ids) > 0 ? aws_instance.app[0].id : ""
 }
