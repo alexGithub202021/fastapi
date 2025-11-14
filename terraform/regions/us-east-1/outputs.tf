aafeb77f283a8c7cfd0b0f3e1a84c63a88f5185e
Work <alexnguyen@Alexs-MacBook-Air.local>
2025-11-14 23:28:06 +0700

    fix TF output resource index, following count use in config block

--- terraform/regions/us-east-1/outputs.tf
+++ terraform/regions/us-east-1/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
-  value = aws_instance[0].app.public_ip
+  value = aws_instances.app[0].public_ip
 }
 
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value = aws_instance[0].app.id
+  value = aws_instances.app[0].id
 }
